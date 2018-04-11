4e1581d3e8874fb267c7651f7c6dbac2266a0b61
wangwei108 <wangwei108@lenovo.com>
2018-04-11 08:47:40 +0800

    add login

--- src/main/java/com/dev/msg/controller/UserInfoController.java
+++ src/main/java/com/dev/msg/controller/UserInfoController.java
@@ -1,9 +1,13 @@
 package com.dev.msg.controller;
 
+import com.dev.msg.utils.ResultVOUtil;
 import com.dev.msg.vo.ResultVO;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.HashMap;
 
 /**
  * @author: wangwei
@@ -15,9 +19,18 @@ import org.springframework.web.bind.annotation.RestController;
 public class UserInfoController {
 
 
-//    @PostMapping("/login")
-//    public ResultVO login(){
-//
-//    }
+    @PostMapping("/login")
+    public ResultVO login(@RequestBody HashMap<String,String> user){
+
+        String userId= user.get("userId");
+        String userPwd = user.get("userPwd");
+
+        return ResultVOUtil.success();
+
+    }
+
+    public ResultVO logout(){
+        return ResultVOUtil.success();
+    }
 
 }
